d1561302aa08b4ab03e6114f335b291f3c1df538
KazimierzMigdal <kazmierz.migdal@protonmail.com>
2020-09-11 14:31:51 +0200

    Shop page Route added to App component

--- src/App.js
+++ src/App.js
@@ -3,19 +3,16 @@ import './App.css';
 import { Route, Switch } from 'react-router-dom';
 
 import HomePage from './pages/homepage/homepage.component.js';
+import ShopPage from './pages/shop/shop.component.js';
 
-const HatsPage = () =>(
-  <div>
-    <h1>lalala</h1>
-  </div>
-    );
+
 
 function App() {
   return (
     <div>
       <Switch>
         <Route exact path='/' component={HomePage} />
-        <Route exact path='/hats' component={HatsPage} />
+        <Route exact path='/shop' component={ShopPage} />
       </Switch>
     </div>
   );
