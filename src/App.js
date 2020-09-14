2234c0c92faf2aa8ed41640e597237d5853da049
KazimierzMigdal <kazmierz.migdal@protonmail.com>
2020-09-14 11:05:27 +0200

    Header component added to App.js

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import './App.css';
 import { Route, Switch } from 'react-router-dom';
 
 import HomePage from './pages/homepage/homepage.component.js';
+import Header from './components/header/header.component.js';
 import ShopPage from './pages/shop/shop.component.js';
 
 
@@ -10,6 +11,7 @@ import ShopPage from './pages/shop/shop.component.js';
 function App() {
   return (
     <div>
+      <Header />
       <Switch>
         <Route exact path='/' component={HomePage} />
         <Route exact path='/shop' component={ShopPage} />
