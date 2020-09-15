c3ffe95281198c3c5488b610488dd35bab7c5442
KazimierzMigdal <kazmierz.migdal@protonmail.com>
2020-09-15 16:20:45 +0200

    Register user in firebase after SignInByGoogle

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ import Header from './components/header/header.component.js';
 import ShopPage from './pages/shop/shop.component.js';
 import SignInAndSignUpPage from './pages/sign-in-and-sign-up/sign-in-and-sign-up.component.js';
 
-import { auth } from './firebase/firebase.utils.js';
+import { auth, createUserProfileDocument } from './firebase/firebase.utils.js';
 
 
 
@@ -23,8 +23,22 @@ class App extends React.Component {
   unsubscribeFromAuth = null;
 
   componentDidMount() {
-    this.unsubscribeFromAuth = auth.onAuthStateChanged(user =>{
-      this.setState({ currentUser: user });
+    this.unsubscribeFromAuth = auth.onAuthStateChanged(async userAuth =>{
+      if (userAuth) {
+        const userRef = await createUserProfileDocument(userAuth);
+
+        userRef.onSnapshot(snapShot =>{
+          this.setState({
+            currentUser: {
+              id: snapShot.id,
+              ...snapShot.data()
+            }
+          })
+        })
+      } else {
+        this.setState({ currentUser: userAuth })
+      }
+
     })
   }
 
